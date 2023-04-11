6cdb0e0206ceb325b5ceb99989fd8a5aa9e940cf
Montukhan786 <montukhanpathan@gmail.com>
2023-04-11 12:04:47 +0530

    submission of ATM Interface

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        BankAccount bankAccount = new BankAccount("Muntjirkhan","6942");
+        bankAccount.checkId();
+    }
+}
